63ed99e3914a13b588115a7aad68f503f53e36ab
Kirill Gribov <kirilkadurilka@gmail.com>
2022-04-13 08:41:53 +0300

    Update jsonb dsl. add snapshot publishing (#10)
    * Update jsonb dsl. add snapshot publishing
    
    * fix release version check

--- buildSrc/src/main/kotlin/CI.kt
+++ buildSrc/src/main/kotlin/CI.kt
@@ -1,21 +1,9 @@
 object Ci {
 
-    private const val snapshotBase = "0.0.1"
-
-    private val githubBuildNumber = System.getenv("GITHUB_RUN_NUMBER")
-
-    private val snapshotVersion = when (githubBuildNumber) {
-        null -> "$snapshotBase-LOCAL"
-        else -> "$snapshotBase.${githubBuildNumber}-SNAPSHOT"
-    }
-
-    private val snapshotGradleVersion = when (githubBuildNumber) {
-        null -> "$snapshotBase-LOCAL"
-        else -> "$snapshotBase.${githubBuildNumber}"
-    }
+    private const val snapshotBase = "0.0.2"
 
     private val releaseVersion = System.getenv("RELEASE_VERSION")
 
-    val isRelease = releaseVersion != null
-    val publishVersion = releaseVersion ?: snapshotVersion
+    val isRelease = releaseVersion != null || releaseVersion == ""
+    val publishVersion = releaseVersion ?: "$snapshotBase-SNAPSHOT"
 }
