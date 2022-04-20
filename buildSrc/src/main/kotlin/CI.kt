75d709d7f3f5c542ffea9c1b04f7d59010114f6c
Kirill <kirilkadurilka@gmail.com>
2022-04-20 16:00:54 +0300

    Publish snapshot with git sha version

--- buildSrc/src/main/kotlin/CI.kt
+++ buildSrc/src/main/kotlin/CI.kt
@@ -3,7 +3,8 @@ object Ci {
     private const val snapshotBase = "0.0.2"
 
     private val releaseVersion = System.getenv("RELEASE_VERSION")
+    private val githubSha = System.getenv("GITHUB_SHA") ?: "latest"
 
     val isRelease = releaseVersion != null || releaseVersion == ""
-    val publishVersion = releaseVersion ?: "$snapshotBase-SNAPSHOT"
+    val publishVersion = releaseVersion ?: "$snapshotBase-$githubSha-SNAPSHOT"
 }
