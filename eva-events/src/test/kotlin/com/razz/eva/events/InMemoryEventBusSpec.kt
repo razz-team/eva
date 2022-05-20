0e13ad753d4f52bffd021af286f9e1de5236dfeb
Sergey Prytkov <sergey@razz.team>
2022-05-20 18:54:16 +0600

    cleanup spec

--- eva-events/src/test/kotlin/com/razz/eva/events/InMemoryEventBusSpec.kt
+++ eva-events/src/test/kotlin/com/razz/eva/events/InMemoryEventBusSpec.kt
@@ -18,37 +18,9 @@ import java.util.*
 
 class InMemoryEventBusSpec : FunSpec({
 
-    fun consumer(matcher: suspend (IntegrationModelEvent) -> Unit): Pair<Channel<Result>, EventConsumer> {
-        val chan = Channel<Result> { }
-        return chan to object : EventConsumer {
-            override val modelName = IntegrationModelEvent.ModelName("TestModel")
-            override val eventNames = setOf(IntegrationModelEvent.EventName("TestModelEvent"))
-            override suspend fun consume(event: IntegrationModelEvent) {
-                try {
-                    matcher(event)
-                    chan.send(Result.Ok)
-                } catch (e: Throwable) {
-                    chan.send(Result.Error(e))
-                }
-            }
-        }
-    }
-    fun consumer() = consumer { }.second
-    val uowEvent = UowEvent(
-        id = UowEvent.Id.random(),
-        uowName = UowEvent.UowName("Fake"),
-        principal = TestPrincipal,
-        modelEvents = mapOf(UowEvent.ModelEventId.random() to TestModelEvent),
-        idempotencyKey = IdempotencyKey.random(),
-        params = buildJsonObject {
-            put("id", "some-id")
-        },
-        occurredAt = Instant.now()
-    )
-
     test("Not started event bus does not accept events") {
         val notStarted = InMemoryEventBus(listOf(consumer()))
-        shouldThrow<IllegalStateException> { notStarted.publish(uowEvent) }
+        shouldThrow<IllegalStateException> { notStarted.publish(uowEvent()) }
     }
 
     test("Started and closed event bus does not accept events") {
@@ -56,10 +28,11 @@ class InMemoryEventBusSpec : FunSpec({
             start()
             close()
         }
-        shouldThrow<IllegalStateException> { startedAndClosed.publish(uowEvent) }
+        shouldThrow<IllegalStateException> { startedAndClosed.publish(uowEvent()) }
     }
 
     test("Event bus distributes event to consumer") {
+        val uowEvent = uowEvent()
         val (modelEventId, modelEvent) = uowEvent.modelEvents.entries.first()
         val (chan, consumer) = consumer { event ->
             event.eventName.toString() shouldBe modelEvent.eventName()
@@ -82,17 +55,33 @@ sealed interface Result {
     object Ok : Result
     data class Error(val e: Throwable) : Result
 }
+
 object TestPrincipal : Principal<UUID> {
     override val id = Principal.Id(UUID.randomUUID())
     override val name = Principal.Name("TestPrincipal")
 }
+
 object TestModelId : ModelId<String> {
     override val id = "TestModelId"
 }
+
 object TestModelEvent : ModelEvent<TestModelId> {
     override val modelId = TestModelId
     override val modelName = "TestModel"
 }
+
+private fun uowEvent(vararg modelEvents: TestModelEvent = arrayOf(TestModelEvent)) = UowEvent(
+    id = UowEvent.Id.random(),
+    uowName = UowEvent.UowName("Fake"),
+    principal = TestPrincipal,
+    modelEvents = modelEvents.associate { UowEvent.ModelEventId.random() to TestModelEvent },
+    idempotencyKey = IdempotencyKey.random(),
+    params = buildJsonObject {
+        put("id", "some-id")
+    },
+    occurredAt = Instant.now()
+)
+
 private suspend fun validateResult(chan: Channel<Result>) {
     when (val res = withTimeout(5.toDuration(DurationUnit.SECONDS)) {
         chan.receive()
@@ -101,3 +90,21 @@ private suspend fun validateResult(chan: Channel<Result>) {
         Result.Ok -> {}
     }
 }
+
+private fun consumer(matcher: suspend (IntegrationModelEvent) -> Unit): Pair<Channel<Result>, EventConsumer> {
+    val chan = Channel<Result> { }
+    return chan to object : EventConsumer {
+        override val modelName = IntegrationModelEvent.ModelName("TestModel")
+        override val eventNames = setOf(IntegrationModelEvent.EventName("TestModelEvent"))
+        override suspend fun consume(event: IntegrationModelEvent) {
+            try {
+                matcher(event)
+                chan.send(Result.Ok)
+            } catch (e: Throwable) {
+                chan.send(Result.Error(e))
+            }
+        }
+    }
+}
+
+private fun consumer() = consumer { }.second
