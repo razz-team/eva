b85aeab55d78ed34d2c0236e0b23fb0fb6af837c
Sergey Prytkov <sergey@razz.team>
2022-05-22 21:21:52 +0600

    attempt to fix logs

--- eva-events/build.gradle.kts
+++ eva-events/build.gradle.kts
@@ -8,5 +8,7 @@ dependencies {
     api(libs.kotlin_coroutines)
     api(project(eva.eva_domain))
     implementation(libs.kotlin_logging)
+    implementation(libs.kotlin_slf4)
+    implementation(libs.logback)
     implementation(project(eva.eva_serialization))
 }
