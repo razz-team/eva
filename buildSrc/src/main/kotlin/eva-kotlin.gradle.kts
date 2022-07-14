b58e0996e188fec870bc7dab30db5a13677b2885
Sergey Prytkov <sergey@razz.team>
2022-07-14 19:42:59 +0300

    PLATFORM-XX 2022 july versions bump

--- buildSrc/src/main/kotlin/eva-kotlin.gradle.kts
+++ buildSrc/src/main/kotlin/eva-kotlin.gradle.kts
@@ -45,8 +45,8 @@ java.sourceCompatibility = versions.java
 java.targetCompatibility = versions.java
 
 tasks.compileKotlin {
-    sourceCompatibility = versions.jvm
-    targetCompatibility = versions.jvm
+//    sourceCompatibility = versions.jvm
+//    targetCompatibility = versions.jvm
     kotlinOptions {
         jvmTarget = versions.jvm
         allWarningsAsErrors = true
@@ -55,8 +55,8 @@ tasks.compileKotlin {
 }
 
 tasks.compileTestKotlin {
-    sourceCompatibility = versions.jvm
-    targetCompatibility = versions.jvm
+//    sourceCompatibility = versions.jvm
+//    targetCompatibility = versions.jvm
     kotlinOptions {
         jvmTarget = versions.jvm
         freeCompilerArgs = listOf("-opt-in=kotlin.RequiresOptIn")
