da1831cbb63ee220fea6864b12a7003c7b4bff67
Sergey Prytkov <sergey@razz.team>
2022-12-28 00:27:52 +0600

    PLATFORM-XXXX multiple directories per one migration (#71)

--- eva-migrations/src/main/kotlin/com/razz/eva/migrations/Migration.kt
+++ eva-migrations/src/main/kotlin/com/razz/eva/migrations/Migration.kt
@@ -1,20 +1,25 @@
 package com.razz.eva.migrations
 
-data class Migration(val path: String, val schema: DbSchema) {
+data class Migration(val path: String, val schema: DbSchema, val additionalPaths: List<String>) {
 
-    fun classpathLocation() = "classpath:$path"
+    constructor(path: String, schema: DbSchema, vararg additionalPaths: String) :
+        this(path, schema, additionalPaths.toList())
+
+    fun classpathLocation() = (listOf(path) + additionalPaths)
+        .joinToString(prefix = "classpath:", separator = ",classpath:")
 
     override fun toString() = """
         Migration[
-            path=$path,
+            path=${(listOf(path) + additionalPaths).joinToString()},
             schema=$schema
         ]
     """.trimIndent()
 
     companion object Factory {
 
-        fun modelsMigration(path: String): Migration {
+        fun modelsMigration(path: String, vararg additionalPaths: String): Migration {
             check(!path.contains("events"))
+            check(additionalPaths.all { !it.contains("events") })
             return Migration(path, DbSchema.ModelsSchema)
         }
 
