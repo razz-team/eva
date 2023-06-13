5d5dd2cb3cf84a7273074acc725d365a9dfd04d3
Sergey Prytkov <sergey@razz.team>
2023-06-13 15:51:21 +0100

    BRY-TANK saga spec

--- eva-test/build.gradle.kts
+++ eva-test/build.gradle.kts
@@ -7,12 +7,14 @@ dependencies {
     implementation(project(eva.eva_domain))
     implementation(project(eva.eva_repository))
     implementation(project(eva.eva_uow))
+    implementation(project(eva.eva_saga))
     implementation(project(eva.eva_migrations))
     implementation(project(eva.eva_persistence_jdbc))
     implementation(project(eva.eva_persistence_vertx))
 
     implementation(platform(libs.testcontainers_bom))
 
+    implementation(libs.kotlin_reflect)
     implementation(libs.kotest_framework)
     implementation(libs.postgres)
     implementation(libs.kotest_runner)
