591df4ca965e208eb8d84c0c055f225610c73da3
Sergey Prytkov <sergey@razz.team>
2023-06-19 22:33:03 +0100

    BRY-JOOQ instant converter test

--- eva-jooq/src/test/kotlin/com/razz/jooq/converter/InstantConverterTest.kt
+++ eva-jooq/src/test/kotlin/com/razz/jooq/converter/InstantConverterTest.kt
@@ -2,7 +2,10 @@ package com.razz.jooq.converter
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import java.sql.Timestamp
 import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset.UTC
 
 class InstantConverterTest : FunSpec({
 
@@ -11,6 +14,7 @@ class InstantConverterTest : FunSpec({
     test("Millis should not be added") {
         val instantWithMillis = Instant.parse("2021-06-04T15:54:30.123Z")
         val timestampWithMillis = converter.to(instantWithMillis)!!
+        timestampWithMillis shouldBe Timestamp.valueOf(LocalDateTime.ofInstant(instantWithMillis, UTC))
         timestampWithMillis.nanos shouldBe 123_000_000
     }
 })
