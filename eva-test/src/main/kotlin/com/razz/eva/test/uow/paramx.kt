02c705579d641e16aa076a84c4d7beb7ff093fbc
Sergey Prytkov <sergey@razz.team>
2023-07-03 15:04:41 +0100

    remove exts

---
+++ eva-test/src/main/kotlin/com/razz/eva/test/uow/paramx.kt
@@ -0,0 +1,7 @@
+@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
+package com.razz.eva.test.uow
+
+import com.razz.eva.uow.InstantiationContext
+
+val INITIAL = InstantiationContext(0)
+val AFTER_RETRY = InstantiationContext(1)
