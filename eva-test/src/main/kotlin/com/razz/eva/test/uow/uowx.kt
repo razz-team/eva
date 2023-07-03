c0cead1ae59d89e57125bcd72e59fe3453ce01ee
Sergey Prytkov <sergey@razz.team>
2023-07-03 12:27:48 +0100

    concurrent param slot

--- eva-test/src/main/kotlin/com/razz/eva/test/uow/uowx.kt
+++ eva-test/src/main/kotlin/com/razz/eva/test/uow/uowx.kt
@@ -6,6 +6,7 @@ import com.razz.eva.uow.UowParams
 import io.mockk.CapturingSlot
 import io.mockk.MockK
 import io.mockk.MockKDsl
+import java.util.concurrent.CopyOnWriteArrayList
 
 inline fun <reified T : UowParams<T>> paramsSlot(): CapturingSlot<InstantiationContext.() -> T> {
     return MockK.useImpl {
@@ -15,4 +16,17 @@ inline fun <reified T : UowParams<T>> paramsSlot(): CapturingSlot<InstantiationC
 
 fun <T : UowParams<T>> CapturingSlot<InstantiationContext.() -> T>.captured(): T {
     return (captured)(InstantiationContext(0))
+}
+
+inline fun <reified T : UowParams<T>> concurrentSlot() = CopyOnWriteArrayList<InstantiationContext.() -> T>()
+
+val <T : UowParams<T>> CopyOnWriteArrayList<InstantiationContext.() -> T>.captured: T get(): T {
+    return (first())(InstantiationContext(0))
+}
+
+infix fun <T : UowParams<T>> CopyOnWriteArrayList<InstantiationContext.() -> T>.shouldCapture(matcher: T.() -> Unit) {
+    this.any { captured ->
+        matcher((captured)(InstantiationContext(0)))
+        true
+    }
 }
