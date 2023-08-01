50219131674d1d1b4a9f0059a2cce95f1c8c9248
Sergey Prytkov <sergey@razz.team>
2023-08-01 18:55:25 +0100

    try system property

--- buildSrc/src/main/kotlin/CI.kt
+++ buildSrc/src/main/kotlin/CI.kt
@@ -1,9 +1,11 @@
+import java.lang.Boolean.parseBoolean
+
 object Ci {
 
     private const val SNAPSHOT_BASE = "0.4.0"
     private const val RELEASE_VERSION = "0.3.0"
     private val githubSha = System.getenv("GITHUB_SHA") ?: "latest"
 
-    val publishRelease = true // System.getenv("PUBLISH_RELEASE")?.let(::parseBoolean) ?: false
+    val publishRelease = System.getProperty("release", "false").let(::parseBoolean)
     val publishVersion = if (publishRelease) RELEASE_VERSION else "$SNAPSHOT_BASE-$githubSha-SNAPSHOT"
 }
