06297532e063071ede54f75fd0a9b35cb9040a31
Sergey Prytkov <sergey@razz.team>
2023-08-01 19:40:24 +0100

    EVA-RELEASE inverse defaults

--- buildSrc/src/main/kotlin/CI.kt
+++ buildSrc/src/main/kotlin/CI.kt
@@ -6,6 +6,6 @@ object Ci {
     private const val RELEASE_VERSION = "0.3.0"
     private val githubSha = System.getenv("GITHUB_SHA") ?: "latest"
 
-    val publishRelease = System.getProperty("release", "false").let(::parseBoolean)
+    val publishRelease = System.getProperty("release", "true").let(::parseBoolean)
     val publishVersion = if (publishRelease) RELEASE_VERSION else "$SNAPSHOT_BASE-$githubSha-SNAPSHOT"
 }
