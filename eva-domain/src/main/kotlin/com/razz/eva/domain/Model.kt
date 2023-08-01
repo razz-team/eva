eaeac5fccd0a56a61cc68c0d12c8d5ca18ab7042
Sergey Prytkov <sergey@razz.team>
2023-08-01 18:33:44 +0100

    EVA-MODEL disallow entity state functions overriding in models

--- eva-domain/src/main/kotlin/com/razz/eva/domain/Model.kt
+++ eva-domain/src/main/kotlin/com/razz/eva/domain/Model.kt
@@ -3,7 +3,13 @@ package com.razz.eva.domain
 abstract class Model<ID : ModelId<out Comparable<*>>, E : ModelEvent<ID>>(
     private val id: ID,
     private val entityState: EntityState<ID, E>
-) : Identifiable<ID>, EntityStateMixin<ID, E> by entityState {
+) : Identifiable<ID>, EntityStateMixin<ID, E> {
+
+    final override fun isDirty(): Boolean = entityState.isDirty()
+    final override fun isNew(): Boolean = entityState.isNew()
+    final override fun isPersisted(): Boolean = entityState.isPersisted()
+    final override fun version(): Version = entityState.version()
+    final override fun writeEvents(drive: ModelEventDrive<E>): ModelEventDrive<E> = entityState.writeEvents(drive)
 
     override fun id(): ID = id
 
