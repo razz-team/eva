94a2f278fcd2dbba09023e702df4bb534d2b7b9d
Sergey Prytkov <sergey@razz.team>
2023-08-01 13:46:36 +0100

    EVA-RELEASE 0.1.0

--- buildSrc/src/main/kotlin/CI.kt
+++ buildSrc/src/main/kotlin/CI.kt
@@ -2,9 +2,9 @@ object Ci {
 
     private const val snapshotBase = "0.0.3"
 
-    private val releaseVersion = System.getenv("RELEASE_VERSION")
+    private val releaseVersion = "0.1.0" // used to be System.getenv("RELEASE_VERSION")
     private val githubSha = System.getenv("GITHUB_SHA") ?: "latest"
 
-    val isRelease = releaseVersion != null || releaseVersion == ""
+    val isRelease = !releaseVersion.isNullOrEmpty()
     val publishVersion = releaseVersion ?: "$snapshotBase-$githubSha-SNAPSHOT"
 }
