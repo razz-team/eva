ea33ede4a62fd2e1405416bd074b3648b1b57f6c
Sergey Prytkov <sergey@razz.team>
2023-08-01 18:46:56 +0100

    EVA-RELEASE force push to maven

--- buildSrc/src/main/kotlin/CI.kt
+++ buildSrc/src/main/kotlin/CI.kt
@@ -1,11 +1,9 @@
-import java.lang.Boolean.parseBoolean
-
 object Ci {
 
     private const val SNAPSHOT_BASE = "0.4.0"
     private const val RELEASE_VERSION = "0.3.0"
     private val githubSha = System.getenv("GITHUB_SHA") ?: "latest"
 
-    val publishRelease = System.getenv("PUBLISH_RELEASE")?.let(::parseBoolean) ?: false
+    val publishRelease = true // System.getenv("PUBLISH_RELEASE")?.let(::parseBoolean) ?: false
     val publishVersion = if (publishRelease) RELEASE_VERSION else "$SNAPSHOT_BASE-$githubSha-SNAPSHOT"
 }
