8382b653d4040e6919d18dc5daa305035fa28c8d
Sergey Prytkov <sergey@razz.team>
2023-08-01 14:20:55 +0100

    EVA-RELEASE publish both snapshots and release

--- buildSrc/src/main/kotlin/CI.kt
+++ buildSrc/src/main/kotlin/CI.kt
@@ -1,10 +1,11 @@
+import java.lang.Boolean.parseBoolean
+
 object Ci {
 
-    private const val snapshotBase = "0.0.3"
-
-    private val releaseVersion = "0.1.0" // used to be System.getenv("RELEASE_VERSION")
+    private const val SNAPSHOT_BASE = "0.0.3"
+    private const val RELEASE_VERSION = "0.1.0"
     private val githubSha = System.getenv("GITHUB_SHA") ?: "latest"
 
-    val isRelease = !releaseVersion.isNullOrEmpty()
-    val publishVersion = releaseVersion ?: "$snapshotBase-$githubSha-SNAPSHOT"
+    val publishRelease = System.getenv("PUBLISH_RELEASE")?.let(::parseBoolean) ?: false
+    val publishVersion = if (publishRelease) RELEASE_VERSION else "$SNAPSHOT_BASE-$githubSha-SNAPSHOT"
 }
