a5f013fe5ee3123b95e938defac8047cdc2616f7
Sergey Prytkov <sergey@razz.team>
2023-10-01 20:22:02 +0100

    EVA-SPECS generics in uow verification (#107)

--- eva-test/src/main/kotlin/com/razz/eva/test/uow/UowBehaviorSpec.kt
+++ eva-test/src/main/kotlin/com/razz/eva/test/uow/UowBehaviorSpec.kt
@@ -2,14 +2,25 @@ package com.razz.eva.test.uow
 
 import com.razz.eva.uow.Clocks.fixedUTC
 import com.razz.eva.uow.Clocks.millisUTC
+import com.razz.eva.uow.verify.EqualityVerifier
+import com.razz.eva.uow.verify.EqualityVerifierAware
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 
 abstract class UowBehaviorSpec(
     body: UowBehaviorSpec.() -> Unit = {}
-) : BehaviorSpec() {
+) : BehaviorSpec(), EqualityVerifierAware {
 
     val now = millisUTC().instant()
     val clock = fixedUTC(now)
+
+    private object KotestEqualityVerifier : EqualityVerifier {
+        override fun <T> verify(expected: T, actual: T) {
+            expected shouldBe actual
+        }
+    }
+
+    override val equalityVerifier: EqualityVerifier = KotestEqualityVerifier
 
     init {
         this.body()
--- eva-uow/src/main/kotlin/com/razz/eva/uow/verify/UowSpec.kt
+++ eva-uow/src/main/kotlin/com/razz/eva/uow/verify/UowSpec.kt
@@ -2,7 +2,16 @@ package com.razz.eva.uow.verify
 
 import com.razz.eva.domain.Model
 import com.razz.eva.domain.ModelEvent
+import com.razz.eva.domain.ModelId
 import com.razz.eva.uow.Changes
+
+interface EqualityVerifierAware {
+    val equalityVerifier: EqualityVerifier
+}
+
+interface EqualityVerifier {
+    fun <T> verify(expected: T, actual: T)
+}
 
 infix fun <R> Changes<R>.verifyInOrder(block: UowSpec<R>.() -> Unit) {
     val spec = UowSpec(this)
@@ -11,15 +20,19 @@ infix fun <R> Changes<R>.verifyInOrder(block: UowSpec<R>.() -> Unit) {
 }
 
 class UowSpec<R> internal constructor(
-    changes: Changes<R>
+    changes: Changes<R>,
 ) : UowSpecBase<R>(changes) {
 
-    fun returnsEq(expected: R) = returns {
+    fun <RR : R> returnsEq(expected: RR) = returnsAs<RR> {
         check(expected == this) { "Result doesn't match" }
     }
 
     fun returns(verifyResult: R.() -> Unit) {
         verifyResult(verifyResult)
+    }
+
+    fun <RR : R> returnsAs(verifyResult: RR.() -> Unit) {
+        verifyResultAs(verifyResult)
     }
 
     fun <M : Model<*, *>> addsEq(expected: M) {
@@ -32,6 +45,12 @@ class UowSpec<R> internal constructor(
         return verifyAdded(verify)
     }
 
+    fun <M : Model<*, *>> EqualityVerifierAware.adds(id: ModelId<*>, verify: M.() -> Unit): M {
+        val verified = verifyAdded(verify)
+        equalityVerifier.verify(verified.id(), id)
+        return verified
+    }
+
     fun <M : Model<*, *>> updatesEq(expected: M) {
         verifyUpdated<M> { actual ->
             check(actual == expected) { "Got unexpected update of [$actual]" }
@@ -42,6 +61,12 @@ class UowSpec<R> internal constructor(
         return verifyUpdated(verify)
     }
 
+    fun <M : Model<*, *>> EqualityVerifierAware.updates(id: ModelId<*>, verify: M.() -> Unit): M {
+        val verified = verifyUpdated(verify)
+        equalityVerifier.verify(verified.id(), id)
+        return verified
+    }
+
     fun <E : ModelEvent<*>> emitsEq(expected: E) {
         verifyEmitted<E> { actual ->
             check(actual == expected) { "Got unexpected emit of [$actual]" }
@@ -50,5 +75,10 @@ class UowSpec<R> internal constructor(
 
     fun <E : ModelEvent<*>> emits(verify: E.() -> Unit) {
         verifyEmitted(verify)
+    }
+
+    fun <E : ModelEvent<*>> EqualityVerifierAware.emits(id: ModelId<*>, verify: E.() -> Unit) {
+        val verified = verifyEmitted(verify)
+        equalityVerifier.verify(verified.modelId, id)
     }
 }
