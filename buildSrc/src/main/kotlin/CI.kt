71a933edf9fbe7a979f0b8a425277fb7eee9bd51
Sergey Prytkov <sergey@razz.team>
2024-01-17 21:44:48 +0300

    EVA-PIMP relax queries out param (#118)

--- buildSrc/src/main/kotlin/CI.kt
+++ buildSrc/src/main/kotlin/CI.kt
@@ -3,7 +3,7 @@ import java.lang.Boolean.parseBoolean
 object Ci {
 
     private const val SNAPSHOT_BASE = "0.6.0"
-    private const val RELEASE_VERSION = "0.5.5"
+    private const val RELEASE_VERSION = "0.5.6"
     private val githubSha = System.getenv("GITHUB_SHA") ?: "latest"
 
     val publishRelease = System.getProperty("release", "true").let(::parseBoolean)
