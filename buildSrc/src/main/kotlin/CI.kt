7cdc15b962594b52bfcc61f96ee7d30410be5b26
Sergey Prytkov <sergey@razz.team>
2024-05-25 18:55:40 +0100

    EVA-PIMP bump versions may 2024 correct release versions

--- buildSrc/src/main/kotlin/CI.kt
+++ buildSrc/src/main/kotlin/CI.kt
@@ -2,8 +2,8 @@ import java.lang.Boolean.parseBoolean
 
 object Ci {
 
-    private const val SNAPSHOT_BASE = "0.7.1"
-    private const val RELEASE_VERSION = "0.6.8"
+    private const val SNAPSHOT_BASE = "0.7.0"
+    private const val RELEASE_VERSION = "0.6.9"
     private val githubSha = System.getenv("GITHUB_SHA") ?: "latest"
 
     val publishRelease = System.getProperty("release", "true").let(::parseBoolean)
