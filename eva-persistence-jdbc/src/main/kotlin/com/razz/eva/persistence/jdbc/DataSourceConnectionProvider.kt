b8d2c9a84c17181d6ebe11225666bd037f19f521
vooft <52610+vooft@users.noreply.github.com>
2024-09-10 14:04:39 +0400

    Fix leaking jdbc connection

--- eva-persistence-jdbc/src/main/kotlin/com/razz/eva/persistence/jdbc/DataSourceConnectionProvider.kt
+++ eva-persistence-jdbc/src/main/kotlin/com/razz/eva/persistence/jdbc/DataSourceConnectionProvider.kt
@@ -3,9 +3,11 @@ package com.razz.eva.persistence.jdbc
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.withContext
 import java.sql.Connection
 import javax.sql.DataSource
+import kotlin.coroutines.coroutineContext
 
 typealias HikariPoolConnectionProvider = DataSourceConnectionProvider
 
@@ -15,6 +17,8 @@ class DataSourceConnectionProvider(
 ) : JdbcConnectionProvider {
 
     override suspend fun acquire(): Connection {
+        coroutineContext.ensureActive() // explicitly check for cancellation before acquiring a connection
+
         // if withContext is cancelled, regardless of the inner block result, the cancellation exception will be thrown
         // here there is nothing to cancel, and we do want to get the connection back regardless of the cancellation
         return withContext(blockingJdbcContext + NonCancellable) {
