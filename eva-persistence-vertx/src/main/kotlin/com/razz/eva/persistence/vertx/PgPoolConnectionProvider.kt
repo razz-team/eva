fcea434fa76d54c3ddfca965e71e602b8ca2f653
vooft <52610+vooft@users.noreply.github.com>
2024-09-11 09:43:19 +0400

    Make Vert.x connection provider non-cancellable

--- eva-persistence-vertx/src/main/kotlin/com/razz/eva/persistence/vertx/PgPoolConnectionProvider.kt
+++ eva-persistence-vertx/src/main/kotlin/com/razz/eva/persistence/vertx/PgPoolConnectionProvider.kt
@@ -3,16 +3,22 @@ package com.razz.eva.persistence.vertx
 import io.vertx.kotlin.coroutines.coAwait
 import io.vertx.pgclient.PgConnection
 import io.vertx.sqlclient.Pool
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.withContext
 
 class PgPoolConnectionProvider(
     private val pool: Pool
 ) : PgConnectionProvider {
 
     override suspend fun acquire(): PgConnection {
-        return pool.connection.coAwait() as PgConnection
+        return withContext(NonCancellable) {
+            pool.connection.coAwait() as PgConnection
+        }
     }
 
     override suspend fun release(connection: PgConnection) {
-        connection.close().coAwait()
+        withContext(NonCancellable) {
+            connection.close().coAwait()
+        }
     }
 }
