de92955618039f26f3b9d1171e7ab6c90007eb76
Dmitry Kapitun <dmitry.kapitun@brighty.app>
2024-10-07 18:51:29 +0700

    EVA-PAGING let `.asFlow` specify starting page

--- eva-paging/src/main/kotlin/com/razz/eva/paging/Pages.kt
+++ eva-paging/src/main/kotlin/com/razz/eva/paging/Pages.kt
@@ -5,17 +5,19 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.flow
 
-abstract class Pages<ORDER_BY : Comparable<ORDER_BY>, ELEMENT : Any> (
-    private val batchSize: Int
+abstract class Pages<ORDER_BY : Comparable<ORDER_BY>, ELEMENT : Any>(
+    private val batchSize: Int,
 ) {
 
     protected abstract suspend fun batch(page: Page<ORDER_BY>): PagedList<ELEMENT, ORDER_BY>
 
-    fun asFlow(): Flow<ELEMENT> = flow {
-        asBatchFlow().collect { batch -> batch.forEach { emit(it) } }
+    fun asFlow(page: Page<ORDER_BY>? = null): Flow<ELEMENT> = flow {
+        asBatchFlow(page ?: First(Size(batchSize)))
+            .collect { batch -> batch.forEach { emit(it) } }
     }
 
-    fun asBatchFlow(): Flow<List<ELEMENT>> = flow { emitBatch(First(Size(batchSize))) }
+    fun asBatchFlow(page: Page<ORDER_BY>? = null): Flow<List<ELEMENT>> =
+        flow { emitBatch(page ?: First(Size(batchSize))) }
 
     private tailrec suspend fun FlowCollector<List<ELEMENT>>.emitBatch(page: Page<ORDER_BY>?) {
         if (page != null) {
