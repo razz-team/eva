d6e154601934cb3632bd1125c7646a47cb810bff
alecx <alecx.sitex@gmail.com>
2025-07-03 21:55:05 +0000

    wip

--- buildSrc/src/main/kotlin/eva-publish.gradle.kts
+++ buildSrc/src/main/kotlin/eva-publish.gradle.kts
@@ -3,6 +3,7 @@ import java.net.URI
 plugins {
     id("maven-publish")
     id("java")
+    id("io.github.gradle-nexus.publish-plugin")
 }
 
 val sourcesJar by tasks.registering(Jar::class) {
@@ -11,20 +12,21 @@ val sourcesJar by tasks.registering(Jar::class) {
 }
 
 publishing {
-    repositories {
-        maven {
-            name = "MavenCentral"
-            url = if (Ci.publishRelease) {
-                URI("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/content/repositories/releases/")
-            } else {
-                URI("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/content/repositories/snapshots/")
-            }
-            credentials {
-                username = System.getenv("SONATYPE_TOKEN")
-                password = System.getenv("SONATYPE_TOKEN_PASS")
-            }
-        }
-    }
+    sonatype()
+    // repositories {
+    //     maven {
+    //         name = "MavenCentral"
+    //         url = if (Ci.publishRelease) {
+    //             URI("https://s01.oss.sonatype.org/content/repositories/releases/")
+    //         } else {
+    //             URI("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    //         }
+    //         credentials {
+    //             username = System.getenv("SONATYPE_TOKEN")
+    //             password = System.getenv("SONATYPE_TOKEN_PASS")
+    //         }
+    //     }
+    // }
     publications {
         register<MavenPublication>("eva") {
             val javaComponent = (components["java"] as AdhocComponentWithVariants)
@@ -32,6 +34,18 @@ publishing {
             artifact(sourcesJar)
             groupId = "team.razz.eva"
             version = Ci.publishVersion
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        create("MavenCentral") {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+
+            username = System.getenv("SONATYPE_TOKEN")
+            password = System.getenv("SONATYPE_TOKEN_PASS")
         }
     }
 }
