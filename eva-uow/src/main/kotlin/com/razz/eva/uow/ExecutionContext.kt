67e2be77be2d933ae3afea48d67fe51d126598e3
vooft <52610+vooft@users.noreply.github.com>
2025-10-15 16:31:33 +0400

    Open ExecutionContext for creation outside of eva

--- eva-uow/src/main/kotlin/com/razz/eva/uow/ExecutionContext.kt
+++ eva-uow/src/main/kotlin/com/razz/eva/uow/ExecutionContext.kt
@@ -3,7 +3,7 @@ package com.razz.eva.uow
 import io.opentelemetry.api.OpenTelemetry
 import java.time.Clock
 
-data class ExecutionContext internal constructor(
-    internal val clock: Clock,
-    internal val otel: OpenTelemetry,
+data class ExecutionContext(
+    val clock: Clock,
+    val otel: OpenTelemetry,
 )
