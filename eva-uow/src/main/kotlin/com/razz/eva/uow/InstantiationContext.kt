c7e4058c69b2a3f5beb52306175737d3b927ae5b
Sruli Herskovic <srulih@Srulis-MacBook-Pro.local>
2025-10-20 19:50:32 +0100

    fds

--- eva-uow/src/main/kotlin/com/razz/eva/uow/InstantiationContext.kt
+++ eva-uow/src/main/kotlin/com/razz/eva/uow/InstantiationContext.kt
@@ -1,5 +1,8 @@
 package com.razz.eva.uow
 
+import com.razz.eva.repository.ModelRepos
+
 data class InstantiationContext internal constructor(
     internal val attempt: Int,
+    val repos: ModelRepos
 )
