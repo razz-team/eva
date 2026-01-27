e23bd3df23462ca869fe85957123c304ccdb7565
Sergey Prytkov <sergey@razz.team>
2026-01-27 11:18:36 +0000

    WritableEntityRepository for specs

---
+++ eva-test/src/main/kotlin/com/razz/eva/test/repository/WritableEntityRepository.kt
@@ -0,0 +1,60 @@
+package com.razz.eva.test.repository
+
+import com.razz.eva.domain.CreatableEntity
+import com.razz.eva.domain.DeletableEntity
+import com.razz.eva.persistence.ConnectionMode.REQUIRE_NEW
+import com.razz.eva.persistence.TransactionManager
+import com.razz.eva.repository.EntityRepos
+import com.razz.eva.repository.TransactionalContext
+import java.time.Clock
+import java.time.Instant
+
+class WritableEntityRepository(
+    private val txnManager: TransactionManager<*>,
+    private val clock: Clock,
+    private val entityRepos: EntityRepos,
+) {
+
+    suspend fun <R> inTransaction(
+        txStartedAt: Instant,
+        block: suspend (context: TransactionalContext) -> R,
+    ): R {
+        val context = TransactionalContext.transactionalContext(txStartedAt)
+        val txBlock: suspend () -> R = {
+            block(context)
+        }
+        return txnManager.inTransaction(REQUIRE_NEW, txBlock)
+    }
+
+    suspend fun <E : CreatableEntity> add(
+        entity: E,
+        txStartedAt: Instant = clock.instant(),
+    ): E =
+        inTransaction(txStartedAt) {
+            entityRepos.repoFor(entity).add(it, entity)
+        }
+
+    suspend fun <E : CreatableEntity> add(
+        entities: List<E>,
+        txStartedAt: Instant = clock.instant(),
+    ): List<E> =
+        inTransaction(txStartedAt) { context ->
+            entities.map { entityRepos.repoFor(it).add(context, it) }
+        }
+
+    suspend fun <E : DeletableEntity> delete(
+        entity: E,
+        txStartedAt: Instant = clock.instant(),
+    ): Boolean =
+        inTransaction(txStartedAt) {
+            entityRepos.deletableRepoFor(entity).delete(it, entity)
+        }
+
+    suspend fun <E : DeletableEntity> delete(
+        entities: List<E>,
+        txStartedAt: Instant = clock.instant(),
+    ): Int =
+        inTransaction(txStartedAt) { context ->
+            entities.count { entityRepos.deletableRepoFor(it).delete(context, it) }
+        }
+}
