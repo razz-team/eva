80b724e6eee815c03a85014ce8a53a400043f186
vooft <52610+vooft@users.noreply.github.com>
2026-03-04 11:00:24 +0400

    Split release pipeline from the build pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,5 +17,5 @@ tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 allprojects {
     group = "team.razz.eva"
-    version = Ci.publishVersion
+    version = System.getenv("RELEASE_VERSION") ?: System.getenv("GITHUB_SHA") ?: "1.0-SNAPSHOT"
 }
