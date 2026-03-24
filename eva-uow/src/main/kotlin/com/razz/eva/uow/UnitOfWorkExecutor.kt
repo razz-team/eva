a5f74b188526b675c554d48bef2d5945368cc98a
vooft <52610+vooft@users.noreply.github.com>
2026-03-24 12:33:07 +0400

    Fix uninitialized lateinit exception when UoW was not registered

--- eva-uow/src/main/kotlin/com/razz/eva/uow/UnitOfWorkExecutor.kt
+++ eva-uow/src/main/kotlin/com/razz/eva/uow/UnitOfWorkExecutor.kt
@@ -56,6 +56,7 @@ class UnitOfWorkExecutor(
             ?: throw IllegalArgumentException("Attempted to register multiple factories for ${it.key.simpleName}")
     }
 
+    @Deprecated("Use execute with explicit UoW")
     suspend fun <PRINCIPAL, PARAMS, RESULT, UOW> execute(
         principal: PRINCIPAL,
         uowFactory: (ExecutionContext) -> UOW,
