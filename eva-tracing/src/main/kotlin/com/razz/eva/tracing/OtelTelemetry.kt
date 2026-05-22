22f69b105590b4deb5be09218a0b0d2964da5ecf
vooft <52610+vooft@users.noreply.github.com>
2026-05-22 19:28:33 +0400

    Add spans for jdbc semaphore and connection acquire (#241)

--- eva-tracing/src/main/kotlin/com/razz/eva/tracing/OtelTelemetry.kt
+++ eva-tracing/src/main/kotlin/com/razz/eva/tracing/OtelTelemetry.kt
@@ -1,7 +1,27 @@
 package com.razz.eva.tracing
 
 import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.api.metrics.Meter
+import io.opentelemetry.api.trace.SpanBuilder
+import io.opentelemetry.api.trace.Tracer
 
-fun OpenTelemetry.getEvaTracer() = getTracer("eva")
+fun OpenTelemetry.getEvaTracer(): Tracer = getTracer("eva")
 
-fun OpenTelemetry.getEvaMeter() = getMeter("eva")
+fun OpenTelemetry.getEvaMeter(): Meter = getMeter("eva")
+
+suspend fun <T> OpenTelemetry?.withSpan(
+    spanName: String,
+    parameters: (SpanBuilder.() -> Unit)? = null,
+    block: suspend () -> T,
+): T {
+    val evaTracer = this?.getEvaTracer() ?: return block()
+
+    val span = evaTracer
+        .spanBuilder(spanName)
+        .apply { parameters?.invoke(this) }
+        .startSpan()
+
+    return span.use {
+        block()
+    }
+}
